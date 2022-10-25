16ddcb37883771c803900a2900a3d121b1598c01
tenshi-cs <116143418+tenshi-cs@users.noreply.github.com>
2022-10-25 17:01:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,28 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "10247139"
+API_ID = "8858279"
 
 API_HASH = "96b46175824223a33737657ab943fd6a"
 
-BOT_TOKEN = "5690078553:AAGTChZ9wX1BONinAOfdMdZkN1cAHoMPLRw"
+BOT_TOKEN = "ef28c3f458143cbcb4271a98a2e9d596"
 
 MONGO_DB_URI = "mongodb+srv://480p:1234@cluster0.3h5wx8u.mongodb.net/?retryWrites=true&w=majority"
 
 INDEX_ID = -1001159872623
 
-UPLOADS_ID = -1001863513396
+UPLOADS_ID = -1001749720835
 
-PROGRESS_ID = -1001612459325
+PROGRESS_ID = -1001892973424
 
-LINK_ID = -1001822793615
+LINK_ID = -1001816606511
 
 STATUS_ID = 3
 
 SCHEDULE_ID = 15849
 
-CHANNEL_TITLE = "Airing animes hardsub"
+CHANNEL_TITLE = "Animxt"
 
-INDEX_USERNAME = "animext720p_posters"
+INDEX_USERNAME = "Latest_ongoing_airing_anime"
 
-UPLOADS_USERNAME = "animext720p_posters"
+UPLOADS_USERNAME = "Latest_ongoing_airing_anime"
