129170a4798f948ba5aec7fde9cfb0a15a3b2662
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-17 23:15:33 +0530

    Update utils.py

--- main/modules/utils.py
+++ main/modules/utils.py
@@ -217,7 +217,7 @@ ETA: {}`
         
         speed = str(speed) + "x"
 
-        name = name.replace("(480p)", "") + "\n" + "✓  `480p x264 Web-DL`" + "\n" + "✓  `English Sub`" + "\n" + tit + "\n" + "#Source #WebDL"
+        name = name.replace("480p", "480p x264 Web-DL")
       
         text2 = text2.format(
             name,
