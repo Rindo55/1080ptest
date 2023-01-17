4c2c3cf22c09732a02870b4b0523c87c7fe3131c
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-17 23:58:47 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -153,7 +153,7 @@ async def start_uploading(data):
         
         os.rename(file,"video.mkv")
 
-        compressed = await compress_video(duration,videox,name,tit)
+        compressed = await compress_video(duration,videox,guessname)
         
         dingdong = await videox.edit(guessname)
 
