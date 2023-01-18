10e0a96d5c05e50afd428aceead2ff2ef61a2bfc
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-18 10:22:23 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -69,10 +69,10 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
 
             ])
             filed = os.path.basename(file)
-            filed = filed.replace("(480p)", "[480p x265]")
+            filed = filed.replace("(720p)", "[720p x264]")
             caption = f"{name}"
-            caption = caption.replace("(480p)", "")
-            gcaption=f"**{caption}**" + "\n" + "✓  `480p x264 10Bit`" + "\n" + "✓  `English Sub`" + "\n" + f"__({tit})__" + "\n" + "#Encoded #x264"
+            caption = caption.replace("(720p)", "")
+            gcaption=f"**{caption}**" + "\n" + "✓  `720p x264`" + "\n" + "✓  `English Sub`" + "\n" + f"__({tit})__" + "\n" + "#Encoded #x264"
             kayo_id = -1001723145599
             x = await app.send_document(
 
