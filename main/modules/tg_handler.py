33de4752a89f2875955b3b9c12dfa40936d1eb74
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-18 09:19:44 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -152,15 +152,15 @@ async def start_uploading(data):
         videox_id = int(videox_id)
         
         os.rename(file,"video.mkv")
-        try:
 
-            anilist = await get_channel(id)
 
-            if anilist == 0:
+        anilist = await get_channel(id)
 
-               img, caption = await get_anilist_data(name)
+        if anilist == 0:
 
-               main = await app.send_photo(INDEX_ID,photo=img,caption=caption)
+         img, caption = await get_anilist_data(name)
+
+         main = await app.send_photo(INDEX_ID,photo=img,caption=caption)
         compressed = await compress_video(duration,videox,name,guessname)
         
         dingdong = await videox.edit(guessname)
