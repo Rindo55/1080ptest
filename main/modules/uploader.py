cdb0f9e0f2c6702ef805e89f414cbec5986d2858
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-18 18:58:32 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -101,26 +101,6 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
             )
 
             ) 
-        files = {'file': open(file, 'rb')}
-        nanix = await x.edit(gcaption + "\n" "━━━━━━━━━━━━━━━━━━━" + "\n" + "`Generating Link`**", parse_mode = "markdown")
-        callapi = requests.post("https://api.filechan.org/upload", files=files)
-        text = callapi.json()
-        long_url = text['data']['file']['url']['full']
-        api_url = f"https://tnlink.in/api?api=fea911843f6e7bec739708f3e562b56184342089&url={long_url}&format=text"
-        result = requests.get(api_url)
-        nai_text = result.text
-        da_url = "https://da.gd/"
-        url = nai_text
-        shorten_url = f"{da_url}shorten"
-        response = requests.get(shorten_url, params={"url": url})
-        nyaa_text = response.text.strip()                                     
-        output = f"""
-{gcaption}
-━━━━━━━━━━━━━━━━━━━
-[🔗Download Link]({nyaa_text})"""
-        daze = await x.edit(output, parse_mode = "markdown")
-    except Exception:
-       await app.send_message(message.chat.id, text="Something Went Wrong!")
         
     try:
 
