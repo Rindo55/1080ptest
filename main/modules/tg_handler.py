518c6e1882161f3fdd78fe16fb7616a0280638e5
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-21 12:24:56 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -135,7 +135,9 @@ async def start_uploading(data):
         ghostname = ghostname.replace("(1080p)", "")
         
         guessname = f"**{ghostname}**" + "\n" + "✓  `1080p x264 Web-DL`" + "\n" + "✓  `English Sub`" + "\n" + f"__({tit})__" + "\n"+ "#Source #WebDL"
-        videox = await app.send_document(
+        
+        
+        videox = await app.on_message(filters.chat_id(KAYO_ID) & filters.regex('720p')) send_document(
 
                 KAYO_ID,
 
