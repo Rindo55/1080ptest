b27e27d2e1222a44829c69bb495c09a3d0ef3f32
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-26 17:01:33 +0530

    Update compressor.py

--- main/modules/compressor.py
+++ main/modules/compressor.py
@@ -11,7 +11,7 @@ import math
 import subprocess
 
 async def gg():
-     cmd = '''ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animxt':fontsize=25:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -c:v libx265 -preset medium -pix_fmt yuv420p10le -r 24000/1001 -crf 24 -x265-params deblock=-1,-1:no-sao:aq-mode=2:aq-strength=0.90:frame-threads=4:no-info=1 -c:a libopus -b:a 96k "out.mkv" -y''',
+     cmd = '''ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animxt':fontsize=25:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -c:v libx265 -s 1280x720 -profile:v high10-intra -preset medium -r 24000/1001 -crf 24 -x265-params deblock=-1,-1:no-sao:aq-mode=2:aq-strength=0.90:frame-threads=4:no-info=1 -c:a libopus -b:a 96k "out.mkv" -y''',
      subprocess.Popen(cmd,shell=True)
 async def compress_video(total_time, videox, name, guessname):
 
