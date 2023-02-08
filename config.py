69c1a29b110da8f77925d66cba0ee9418b2442a7
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-08 14:33:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "96b46175824223a33737657ab943fd6a"
 
 BOT_TOKEN = "5706217182:AAELnRWjsHatV-yJ5MvNgb7yAz2-QF6EWf8"
 
-MONGO_DB_URI = "mongodb+srv://1080p:linux@cluster0.pyalsti.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://1080p:linode@cluster0.xdkaetb.mongodb.net/?retryWrites=true&w=majority"
 INDEX_ID = -1001884130912
 
 UPLOADS_ID = -1001884130912
